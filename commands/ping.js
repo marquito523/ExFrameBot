44c8ebf7fb09dbde504dc04fb4d4399b101b0246
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-01-15 21:20:40 +0100

    Delete commands directory

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'show an example of ping command',
-    execute(message, args) {
-        return message.channel.send("pong!")
-    },
-
-}
--- commands/yes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'maybe',
-    description: 'Rates a user',
-    execute(message, args) {
-        message.channel.send("yes ofc")
-
-    }
-    }
