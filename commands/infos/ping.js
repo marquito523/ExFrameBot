0295e7f507bce348c6aaa77d5f116d0fd725d955
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-01-23 12:53:39 +0100

    Update ping.js

--- commands/infos/ping.js
+++ commands/infos/ping.js
@@ -7,6 +7,6 @@ module.exports = {
     execute(client, message) {
 
         if (!message.guild.me.hasPermission("EMBED_LINKS")) return message.channel.send("I require `EMBED_LINKS` permission to be able to function")
-        message.channel.send(`${client.emotes.success} - Ping : **${client.ws.ping}ms** !`);
+        message.channel.send(`${client.emotes.success} - Ping : **${client.ws.ping}ms** Api Ping: ${this.client.ws.ping}!`);
     },
 };
