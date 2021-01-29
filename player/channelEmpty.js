628e0b8f81c1ac465e1bf1230ba7c07d25dddfa4
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-01-29 22:42:39 +0100

    Delete player directory

--- player/channelEmpty.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = (client, message, queue) => {
-    if (!message.channel.permissionsFor(message.client.user).has('SEND_MESSAGES'))try{ return message.author.send(new Discord.MessageEmbed().setTitle("Discord Permissions").setURL("https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-Permissions-").setDescription(`ExFrame could not send messages into ${message.channel} because he doesn't have \`SEND_MESSAGES\` permissions.`)) }catch(error){return message.author.send(":x: An unexpected error occiered. We are investigating...")}
-
-    message.channel.send(new Discord.MessageEmbed().setDescription(`${client.emotes.error} - Music stopped as there is no more member in the voice channel !`));
-};
