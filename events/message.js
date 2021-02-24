a7c5b51d1bba7ae780ca6d30746a40b35b945666
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-02-24 16:51:09 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,8 +1,12 @@
+const { relativeTimeRounding } = require("moment");
 const mongodb = require("mongoose")
   
 const Guild = require("../commands/Models/Guild")
 
-const { defaultprefix, Enable_Console_Write_Commmand } = require("../config.json")
+const { defaultprefix, Enable_Console_Write_Commmand } = require("../config.json");
+
+const Throll = new Map();
+
 
 module.exports = async (client, message) => {
 
@@ -15,9 +19,8 @@ module.exports = async (client, message) => {
     if(message.author.id === client.user.id) return;
 
     if (message.author.bot || message.channel.type === 'dm') return;
-    
 
-   let prefix
+    let prefix
 
     let Pguild
 
@@ -59,7 +62,37 @@ module.exports = async (client, message) => {
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
 
     if (cmd){ 
+    if(Throll.get(message.author.id))return message.channel.send("You are executing commands too quicly! Wait a few seconds.")
     cmd.execute(client, message, args)
+    AddThroll(message.author.id, message.author)
     if(Enable_Console_Write_Commmand === true)console.log(`${message.author.username} has run ${cmd.name} command. => Id: ${message.author.id}`)  
     }
 };
+
+
+
+
+
+async function AddThroll(UserID, User){
+    Throll.set(UserID, true)
+    await Throll.get(UserID)
+    setTimeout(() => {
+    RemoveThroll(UserID, User)
+    }, 3000);
+}
+
+async function RemoveThroll(UserID, User){
+    try{
+    return Throll.delete(UserID)
+    }catch(e){
+        if(!Throll.get(UserID)){
+            return
+        }else{
+            try{
+                User.send("If you're seeing this, an **important** bug occiered when executing our anti-spam. We were unable to remove your ID from our cooldown list. This might not affect you. But if your user ID was still inside of our anti-spam, you will not be able to use ExFrame. Please contact us, if this happens via our support server. [Click here](https://discord.gg/6zqN4pWpnm) to get invited to our support server.")
+            }catch(e){
+                return message.channel.send(`An error occiered with ${User}. We cannot remove him from our anti-spam. Screen shot this, and show it in our [support server](https://discord.gg/6zqN4pWpnm) for assistance.`)
+            }
+        }
+    }
+}
