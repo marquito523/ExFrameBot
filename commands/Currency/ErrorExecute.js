70f07940891b1d741a40fee4e58f99787a69ae3d
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-04-18 12:17:30 +0200

    Delete commands directory

--- commands/Currency/ErrorExecute.js
+++
@@ -1,9 +0,0 @@
-const cmd = require("../../Utils/Errors")
-
-module.exports = {
-    name: "entererror",
-    async execute(client, message ,args){
-        const Error = "Hello A new Error"
-        cmd.execute(client, message, args, Error)
-    }
-}
--- commands/Fun/Jonaas.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-module.exports = {
-    name: "jonas",
-    async execute(client, message, args){
-        const prefix = client.prefix
-        return message.channel.send(new Discord.MessageEmbed().setDescription("Hello jonas!"))
-    }
-}
