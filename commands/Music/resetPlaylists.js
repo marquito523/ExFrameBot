4386e71430d3038796c42a3a829c89e547a265cc
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-08-01 14:42:48 +0200

    Update resetPlaylists.js

--- commands/Music/resetPlaylists.js
+++ commands/Music/resetPlaylists.js
@@ -11,7 +11,7 @@ module.exports = {
     name: "resetplaylists",
     async execute(client, message, args) {
 
-        let prefix = client.prefix
+        const prefix = client.prefix
 
         if (!message.channel.permissionsFor(message.client.user).has('SEND_MESSAGES'))try{ return message.author.send(new Discord.MessageEmbed().setTitle("Discord Permissions").setURL("https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-Permissions-").setDescription(`ExFrame could not send messages into ${message.channel} because he doesn't have \`SEND_MESSAGES\` permissions.`)) }catch(error){return message.author.send(":x: An unexpected error occiered. We are investigating...")}
 
