2f8147198841bd745984ce39636e4b98bce10d89
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-08-02 10:55:07 +0200

    Update conf-playlists.js

--- commands/Music/conf-playlists.js
+++ commands/Music/conf-playlists.js
@@ -12,7 +12,7 @@ module.exports = {
     name: "conf-playlists",
     async execute(client, message, args) {
 
-        if (!message.channel.permissionsFor(message.client.user).has('SEND_MESSAGES'))try{ return message.author.send(new Discord.MessageEmbed().setTitle("Discord Permissions").setURL("https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-Permissions-").setDescription(`ExFrame could not send messages into ${message.channel} because he doesn't have \`SEND_MESSAGES\` permissions.`)) }catch(error){return message.author.send(":x: An unexpected error occiered. We are investigating...")}
+        if (!message.channel.permissionsFor(message.client.user).has('SEND_MESSAGES')) try { return message.author.send(new Discord.MessageEmbed().setTitle("Discord Permissions").setURL("https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-Permissions-").setDescription(`ExFrame could not send messages into ${message.channel} because he doesn't have \`SEND_MESSAGES\` permissions.`)) } catch (error) { return message.author.send(":x: An unexpected error occiered. We are investigating...") }
 
         const User = await Playlist.findOne({
 
@@ -30,59 +30,57 @@ module.exports = {
                 })
 
                 newPlaylist.save()
-                .then(result => console.log(result))
-                .catch(err => console.error(err));
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
 
                 return message.channel.send('This server was not in our database! We have added it, please retype this command.');
             }
         });
 
 
-        if(!User)return
+        if (!User) return
 
-        console.log(User)
 
-        if(!User.Public === false && !User.Public === true){
-            try{
- 
+        if (!User.Public === false && !User.Public === true) {
+            try {
+
                 return message.channel.send(new MessageEmbed().setDescription("Please retype the command. An element was missing to your profile. We've just added it!"))
-            }catch(err){
+            } catch (err) {
                 return message.channel.send(new MessageEmbed().setDescription(`Error: ${err}`))
             }
         }
 
         let SetedV
 
-        if(User.Public === false)
-        {SetedV = "**Private**"
-        }else if(User.Public === true)
-        {SetedV = "**Public**"}
-
-        if(SetedV === undefined || SetedV === "undefined"){
-            SetedV = "Error Occured | Your profile State Could Not Be Loaded."
+        if (User.Public === false) {
+            SetedV = "**Private**"
+        } else if (User.Public === true) {
+            SetedV = "**Public**"
         }
 
-        if(!args[0]){
+        if (!args[0]) {
             return message.channel.send(new MessageEmbed().setDescription("Your profile is currently set to " + SetedV + "\n \n To edit it type the following command: \n \n `" + client.prefix + "conf-playlists public / private`"))
         }
 
-       if(!args[0].toLowerCase() === "public" || !args[0].toLowerCase() === "private")return message.channel.send(new MessageEmbed().setDescription("Missing Argument. Please specify a value \n \n **public** or **private**. \n \n **Command use:** `" + client.prefix + "conf-playlists public | private`"))
+        if (!args[0].toLowerCase() === "public" || !args[0].toLowerCase() === "private") return message.channel.send(new MessageEmbed().setDescription("Missing Argument. Please specify a value \n \n **public** or **private**. \n \n **Command use:** `" + client.prefix + "conf-playlists public | private`"))
 
         let Value = false
 
-      if(args[0].toLowerCase() === "private"){Value = false}else if(args[0].toLowerCase() === "public"){Value = true}else{return message.channel.send(new MessageEmbed().setDescription("Unexpected Error."))}
+        console.log(User)
 
-      if(Value === User.Public)return message.channel.send(new MessageEmbed().setDescription(`Your profile is already set to ${SetedV}`))
-try{
-       await User.updateOne({
-           Public: Value
-       })
+        if (args[0].toLowerCase() === "private") { Value = false } else if (args[0].toLowerCase() === "public") { Value = true } else { return message.channel.send(new MessageEmbed().setDescription("Unexpected Error.")) }
 
-       return message.channel.send(new MessageEmbed().setDescription("Succes! Your profile is set to **"+ args[0] + "**"))
+        if (Value === User.Public) return message.channel.send(new MessageEmbed().setDescription(`Your profile is already set to ${SetedV}`))
+        try {
+            await User.updateOne({
+                Public: Value
+            })
 
-    }catch(err){
-        return message.channel.send(`Error: ${err}`)
-    }
+            return message.channel.send(new MessageEmbed().setDescription("Succes! Your profile is set to **" + args[0] + "**"))
+
+        } catch (err) {
+            return message.channel.send(`Error: ${err}`)
+        }
 
     }
 
