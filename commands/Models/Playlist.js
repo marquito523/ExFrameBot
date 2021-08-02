3f8ca87d9eb1cbc4a91f80a1a60912afba9830e6
marquito523 <74208593+marquito523@users.noreply.github.com>
2021-08-02 11:01:37 +0200

    Update Playlist.js

--- commands/Models/Playlist.js
+++ commands/Models/Playlist.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const Playlist = mongoose.Schema({
     UserID: Number,
-    Playlists: Array
+    Playlists: Array,
+    Public: Boolean
 })
 
 module.exports = mongoose.model('Playlist', Playlist);
