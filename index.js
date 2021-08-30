6131bff252d438c337831325d05da936d42ec262
Marco ASKOVIC <74208593+marquito523@users.noreply.github.com>
2021-08-30 09:26:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const discord = require('discord.js');
 const Discord = require('discord.js');
-const quickdb = require("quick.db")
 const { DB_PASSWORD, DB_NAME } = require('./config.json')
 const mongoose = require("mongoose")
 const {MARQPASS, MARQNAME} = require('./config.json')
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }
-const AppDataFile = require("./commands/Models/AppData")
 const GuildMemeberAddFile = require('./commands/Configuration/guildMemberAdd')
 const OnMessageFile = require("./commands/Configuration/MessageEvent")
 const mongodb = require("mongoose")
@@ -14,10 +12,6 @@ const StartUpFile = require("./commands/Configuration/Startup")
 const { defaultprefix, Version } = require("./config.json")
 const client = new discord.Client({ disableEveryone: false });
 const { Player } = require('discord-player');
-const { db } = require('./commands/Models/AppData');
-var FetchingToken = false
-var ConnectedToMongoose = false
-var ConnectionBot = false
 const { token } = require("./config.json")
 
 
@@ -49,7 +43,7 @@ for (const file of player) {
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 client.on('ready', () => {  
-    StartUpFile.execute(client)
+    console.log("yes")
 });
 client.on('guildCreate', guild => {
     guild.systemChannel.send((new Discord.MessageEmbed().setDescription(`**Hello!** \n \n To start using me please type in ${defaultprefix}help \n I'm happy to be with you and I hope I will be able to help you in whatever I can! You view my list of commands in help and following the instructions. To make my help + category work, you will have to do the following => \n **Example**: let's say I want to know about music => ${defaultprefix}help music or moderation ${defaultprefix}help moderation etc... \n \n This is specified in help.\n \n**Useful Links** \n \n **Invite Link:** To invite ExFrame [click here](https://discord.com/api/oauth2/authorize?client_id=733665360188014644&permissions=275901542&scope=bot)  \n \n **Vote ExFrame:** To vote ExFrame on Top.gg [click here](https://top.gg/bot/733665360188014644) \n \n**ExFrame Support Guild:** To join ExFrame's Support Guild [click here](https://discord.gg/6zqN4pWpnm) \n \n **ExFrame Support Website:** To visit ExFrame's Website [click here](https://marquito523.github.io/ExFrame-Website/) \n \n Since you have just added ExFrame to your guild, do you want to start the setup? If yes type "m!start-setup"`).setTitle("**ExFrame**").setTimestamp().setFooter("Introduction to ExFrame").setThumbnail(client.user.displayAvatarURL({ size: 1024 }))));
